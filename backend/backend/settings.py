ce89253e366d3d7147f5c70dc8d54f3c6ea2caa0
MUHAMMMMED <amer66965@gmail.com>
2024-08-19 15:04:32 +0300

    Updat requirements file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY' ) # Use  value if needed
+SECRET_KEY = 'django-insecure-z(83xn=7-#(hjl6vgy+)*cd%y^k*3_f3ak1l9wp-t&6jovm@r6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
